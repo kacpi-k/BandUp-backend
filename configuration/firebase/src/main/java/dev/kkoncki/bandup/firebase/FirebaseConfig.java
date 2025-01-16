8248f3ca71ed85edbf3f8b62ad95c7a239e266aa
Kacper <109245559+kacpi-k@users.noreply.github.com>
2025-01-16 12:44:52 +0100

    added FirebaseConfig

---
+++ configuration/firebase/src/main/java/dev/kkoncki/bandup/firebase/FirebaseConfig.java
@@ -0,0 +1,32 @@
+package dev.kkoncki.bandup.firebase;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Configuration
+public class FirebaseConfig {
+
+    private final FirebaseProps props;
+
+    public FirebaseConfig(FirebaseProps props) {
+        this.props = props;
+    }
+
+    @Bean
+    public FirebaseApp firebaseApp() throws IOException {
+        FileInputStream serviceAccount = new FileInputStream("serviceAccountKey.json");
+
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setStorageBucket(props.getBucket())
+                .build();
+
+        return FirebaseApp.initializeApp(options);
+    }
+}
